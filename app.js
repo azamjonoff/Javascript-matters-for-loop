31c8d5ec04c89b12469a9f5b13ade710b1ea7504
azamjonoff <abdullohazamjonov57gmail.com>
2024-04-24 06:17:06 +0500

    Add 6 matters

---
+++ app.js
@@ -0,0 +1,89 @@
+// const number = prompt(`Raqam kiriting:`);
+
+// for (let i = 1; i < 11; i++) {
+//   console.log(`${number} * ${i} = ${number * i}`);
+// }
+
+// let text =
+//   "Loem ipsaum dolao sit armeat consaecteatu adipaisiciag elita. Saepae, nulla?";
+// let counter = 0;
+
+// for (let i = 0; i < text.length; i++) {
+//   if (text[i].toLowerCase() == "r") {
+//   }
+// }
+
+// let n1 = Number(prompt(`Birinchi raqamni kiriting:`));
+// let belgi = prompt(`Ishorani kiriting:`)
+// let n2 = Number(prompt(`Ikkinchi raqamni kiriting:`));
+
+// let text =
+//   "Lorem ipsum dolo!r sit amet consectetur adipisicin!g elit. E!rror, ipsum!";
+// let newText = "";
+
+// for (let i = 0; i < text.length; i++) {
+//   if (text[i] != "!") {
+//     newText += text[i];
+//   }
+// }
+
+// console.log(newText);
+
+// UYGA VAZIFA START
+
+// 1)
+
+// let k = Number(prompt(`Son kiriting:`));
+// let n = Number(prompt(`Shu son necha marta takrorlanishini hohlaysiz?`));
+
+// for (let i = 0; i <= n; i++) {
+//   console.log(k);
+// }
+
+// 2)
+
+// let a = +prompt(`Birinchi sonni kiriting:`);
+// let b = +prompt(`Ikkinchi sonni kiriting:`);
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+// }
+
+// console.log(`${b - a + 1} ta`);
+
+// 3)
+
+// let a = +prompt(`Birinchi sonni kiriting:`);
+// let b = +prompt(`Ikkinchi sonni kiriting:`);
+
+// for (let i = a + 1; i < b; i++) {
+//   console.log(i);
+// }
+
+// console.log(`${b - a - 1} ta`);
+
+// 4)
+
+// let narxi = +prompt(`Konfet narxini kiriting:`);
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i} kilo ${narxi * i} so'm`);
+// }
+
+// 5)
+
+// let narxi = +prompt(`Konfet narxini kiriting:`);
+
+// for (let i = 0.1; i <= 1; i = i + 0.1) {
+//   console.log(`${i * 1000} gram ${narxi * i} so'm`);
+// }
+
+// 6)
+
+let narxi = (prompt(`Konfet narxini kiriting:`));
+
+for (let i = 1.2; i <= 2; i = i + 0.2) {
+  console.log(`${i} kilo ${narxi * i} so'm`);
+}
+
+// UYGA VAZIFA END
