414e8dabf8bbe7f35f37bad5357fb6eb9ea337ea
azamjonoff <abdullohazamjonov57gmail.com>
2024-04-25 07:38:48 +0500

    ready

--- app.js
+++ app.js
@@ -36,7 +36,7 @@
 // let k = Number(prompt(`Son kiriting:`));
 // let n = Number(prompt(`Shu son necha marta takrorlanishini hohlaysiz?`));
 
-// for (let i = 0; i <= n; i++) {
+// for (let i = 0; i <= n - 1; i++) {
 //   console.log(k);
 // }
 
@@ -80,10 +80,79 @@
 
 // 6)
 
-let narxi = (prompt(`Konfet narxini kiriting:`));
+// let narxi = +prompt(`Konfet narxini kiriting:`);
 
-for (let i = 1.2; i <= 2; i = i + 0.2) {
-  console.log(`${i} kilo ${narxi * i} so'm`);
-}
+// for (let i = 1.2; i <= 2; i = i + 2e-1) {
+//   console.log(`${i} kilo ${narxi * i} so'm`);
+// }
+
+// 7)
+
+// let a = +prompt(`Birinchi sonni kiriting:`);
+// let b = +prompt(`Ikkinchi sonni kiriting:`);
+// let sum = 0;
+
+// for (let i = a; i < b; i++) {
+//   sum += i
+// }
+
+// console.log(`Yigindisi: ${sum}`)
+
+// 8)
+
+// let a = +prompt(`Birinchi sonni kiriting:`);
+// let b = +prompt(`Ikkinchi sonni kiriting:`);
+// let sum = 1;
+
+// for (let i = a; i < b; i++) {
+//   sum *= i
+// }
+
+// console.log(`Ko'paytmasi: ${sum}`)
+
+// 9)
+
+// let a = +prompt(`Birinchi sonni kiriting:`);
+// let b = +prompt(`Ikkinchi sonni kiriting:`);
+// let sum = 0;
+
+// for (let i = a; i < b; i++) {
+//   sum = sum + (i**2)
+// }
+
+// console.log(`Kvadratlarining yigindisi: ${sum}`)
+
+// 10)
+
+// let n = +prompt(`Son kiriting:`);
+// sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + 1 / i;
+// }
+
+// console.log(`Natija: ${sum}`);
+
+// 11)
+
+// let n = +prompt(`Son kiriting:`);
+// sum = 0;
+
+// for (let i = 1; n <= 2*n; i++) {
+//   sum = sum + (n + i) ** 2;
+// }
+
+// console.log(`Natija: ${sum}`);
+
+// 12)
+
+// let n = +prompt(`Son kiriting:`);
+// sum = 1;
+
+// for (let i = 1; 1 < n; i = i + 1e-1) {
+//   sum = sum * i;
+// }
+
+// console.log(`Natija: ${sum}`);
 
 // UYGA VAZIFA END
